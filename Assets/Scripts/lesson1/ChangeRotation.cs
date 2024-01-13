a9feefb247cf7a75f70e161721a1800f66ebdffe
Anushavan Ananyan <>
2024-01-13 18:12:25 +0400

    Added ResetValue function

--- Assets/Scripts/lesson1/ChangeRotation.cs
+++ Assets/Scripts/lesson1/ChangeRotation.cs
@@ -10,54 +10,52 @@ namespace lesson1
         {
             Debug.Log("Awake");
         }
+
         private void OnEnable()
         {
             Debug.Log("OnEnable");
         }
+
         private void Start()
         {
             Debug.Log("Start");
         }
+
         private void Update()
         {
-            if (rotateValue.x > 180)
-            {
-                rotateValue.x = rotateValue.x - 360;
-            }
-            else if(rotateValue.x < -180) 
-            {
-                rotateValue.x = rotateValue.x + 360;
-            }
-            if (rotateValue.y > 180)
-            {
-                rotateValue.y = rotateValue.y - 360;
-            }
-            else if(rotateValue.y < -180) 
-            {
-                rotateValue.y = rotateValue.y + 360;
-            }
-            if (rotateValue.z > 180)
-            {
-                rotateValue.z = rotateValue.z - 360;
-            }
-            else if(rotateValue.z < -180) 
-            {
-                rotateValue.z = rotateValue.z + 360;
-            }
+            ResetValue(ref rotateValue.x);
+            ResetValue(ref rotateValue.y);
+            ResetValue(ref rotateValue.z);
             transform.rotation = Quaternion.Euler(rotateValue.x,rotateValue.y,rotateValue.z);
         }
+
+        private void ResetValue(ref float number) 
+        {
+            if (number > 180)
+            {
+                number = number - 360;
+            }
+            else if(number < -180) 
+            {
+                number = number + 360;
+            }
+        }
+
         private void OnDisable()
         {
             Debug.Log("OnDisable");
         }
+
         private void FixedUpdate()
         {
             
         } 
+
         private void LateUpdate()
         {
             
         }
+        
         private void OnDestroy()
         {
 
